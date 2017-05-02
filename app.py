661448982fbaf25a9482eeeedbb9840c9df68022
nurmemet_209 <nurmemet_209@hotmail.com>
2017-05-02 15:40:39 +0800

    first

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
-from secondprint import custom_func
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
 
-custom_func()
+# Connect to Redis
+redis = Redis(host="redis", db=0)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr('counter')
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv('NAME', "world"), hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80
